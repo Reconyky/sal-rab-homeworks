40a0be127ae3c84693c28742952ef983b9a64f42
Reconyk <reconyk@gmail.com>
2022-05-01 13:11:12 +0300

    add something changes

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,6 +15,8 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
+    // что-то новенькое
+    // и тут что-то новенькое
 
     // Конец решения задания №2.1.
 
