faca07898b717fce5e8338eb259708a3efa0b0ce
Reconyk <reconyk@gmail.com>
2022-05-02 19:10:57 +0300

    Added changes task2

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,19 +5,17 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum;// создайте переменную shippingSum
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
-    // что-то новенькое
-    // и тут что-то новенькое
-
+    if(productsSum = 0)// если productsSum равно 0,
+         console.log(shippingSum = 0);// то shippingSum присвоить значение 0
+    
+    if(productsSum >= freeShippingMinSum)// если productsSum Больше или равна freeShippingMinSum,
+         console.log(shippingSum = 0);// то shippingSum присвоить значение 0
+    
+    if(productsSum > 0 && < freeShippingMinSum)// если productsSum больше 0 и меньше freeShippingMinSum,
+         console.log(shippingSum = shippingPrice);// то shippingSum присвоить значение shippingPrice
+    
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -30,12 +28,13 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    // создайте переменную discountSum
+    let discountSum;// создайте переменную discountSum
 
-    // если productsSum больше или равно discountMinSum,
-    // то присвойте discountSum значение discountPart процентов от productsSum,
-    // иначе присвойте discountSum значение 0
-
+    if(productsSum >= discountMinSum){// если productsSum больше или равно discountMinSum,
+        console.log(discountSum = (discountPart/productsSum));// то присвойте discountSum значение discountPart процентов от productsSum,
+}   else {
+        console.log(discountSum = 0);// иначе присвойте discountSum значение 0
+}
     // Конец решения задания №2.2.
 
     return discountSum;
