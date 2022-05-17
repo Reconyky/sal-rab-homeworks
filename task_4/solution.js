7bfb55034213f07a9dd71b2d37d236e9fec209a3
Reconyk <reconyk@gmail.com>
2022-05-17 15:22:29 +0300

    Added three

--- task_4/solution.js
+++ task_4/solution.js
@@ -5,14 +5,14 @@ function parseProducts(json) {
 
     // Преобразуйте строку json, переданную как аргумент функции,
     let data = JSON.parse(json);// в объект с помощью функции JSON.parse(json)
-    let products = data.products;// и запишите в переменную data
+    let products;
+        products = data.products;// и запишите в переменную data
     return (`${data.products}`)// Верните как результат функции свойство products объекта data
 }
 
 function renderProductsCards(json){// Напишите функцию renderProductsCards(json)
          clearProducts();
-    let products;
-        products = parseProducts(json);
+    let products = parseProducts(json);
         length = products.length;
     for (let i = 0; i < length; i += 1){
         addProducts(products[i]);
