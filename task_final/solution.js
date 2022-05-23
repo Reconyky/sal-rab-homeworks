9cea6d359e9c851e45aeff8dc48daf90a4443253
Daniel <reconyk@gmail.com>
2022-05-23 18:44:41 +0300

    10 added

--- task_final/solution.js
+++ task_final/solution.js
@@ -18,22 +18,22 @@ function sendRequest(name, phone, address, goods, sum) {
     
     let countOfGoods = goods.length;
    
-      for (let i = 0; i < countOfGoods; i++) {
-        data.goods.push(goods[i].title);
-        data.goods.push(goods[i].count);
+      for (let i = 0; i < countOfGoods; i+=1) {
+        data.goods.push(goods[i].title, goods[i].count);
+
     }
 
-    data.order.address = `'ул.' ${address.street}, 'дом' ${address.house}, ${address.entrance} 'подъезд', 
-                         ${address.floor} 'этаж', ${address.flat} 'квартира'`;
+    data.order.address = `'ул.' ${address[street]}, 'дом' ${address[house]}, ${address[entrance]} 'подъезд', 
+                         ${address[floor]} 'этаж', ${address[flat]} 'квартира'`;
         
     data.order.sum = sum; 
 
-    data.client = `${client[name]} + ${client[phone]}`;
+    data.client = `${client.name} + ${client.phone}`;
 
     let jsonData = JSON.stringify({data});
 
     return jsonData;
-};
+}
 
 
 
