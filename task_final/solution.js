aa7f1c8b002a2906387ab7da3d4e46ca530ac494
Daniel <reconyk@gmail.com>
2022-05-23 18:38:17 +0300

    9 added

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,5 +1,5 @@
-// Исправьте функцию sendRequest
-// Аргументы функции:
+// Первая задача
+
 function sendRequest(name, phone, address, goods, sum) {
     
     let data = {
@@ -23,6 +23,49 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push(goods[i].count);
     }
 
+    data.order.address = `'ул.' ${address.street}, 'дом' ${address.house}, ${address.entrance} 'подъезд', 
+                         ${address.floor} 'этаж', ${address.flat} 'квартира'`;
+        
+    data.order.sum = sum; 
+
+    data.client = `${client[name]} + ${client[phone]}`;
+
+    let jsonData = JSON.stringify({data});
+
+    return jsonData;
+};
+
+
+
+// Вторая задача
+
+
+
+function sendRequest(name, phone, address, goods, sum) {
+    
+    let data = {
+          
+        goods: [
+
+          ],
+          
+          order: {
+
+          }
+    };
+
+    
+
+
+
+    
+    let countOfGoods = goods.length;
+   
+      for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push(goods[i].title);
+        data.goods.push(goods[i].count);
+    }
+
     data.order.address = `'ул.' ${address[street]}, 'дом' ${address[house]}, ${address[entrance]} 'подъезд', 
                          ${address[floor]} 'этаж', ${address[flat]} 'квартира'`;
         
@@ -34,43 +77,3 @@ function sendRequest(name, phone, address, goods, sum) {
 
     return jsonData;
 };
-
-// - имя клиента
-// - телефон клиента
-// - объект с адресом доставки: {street, house, entrance, floor, flat}
-// - список товаров в заказе
-// - стоимость заказа с учетом скидок и доставки
-// Как результат функции требуется вернуть JSON,
-// cформированный в соответствии с правилами:
-// Объект data содержит все данные
-// В объекте data есть свойства:
-// - client - строка, имя клиента + телефон клиента;
-// - order - объект, содержащий данные о заказе:
-//     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
-//     - sum - стоимость заказа с учетом скидок и доставки
-// - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
-//     - count - количество в заказе
-// например:
-// {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-//        "sum": 900
-//       },
-//       "goods": [
-//         {
-//           "title": "Пицца",
-//           "count": 2
-//         }
-//      ]
-//    }
-// }
-
- 
-
-
-
-
-
