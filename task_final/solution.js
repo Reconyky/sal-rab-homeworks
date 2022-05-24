bf3d431ac6ac5cf2d2b551109454d68b1bb8d850
Daniel <reconyk@gmail.com>
2022-05-24 14:56:07 +0300

    21 added

--- task_final/solution.js
+++ task_final/solution.js
@@ -16,7 +16,6 @@ function sendRequest(name, phone, address, goods, sum) {
     };
 
     
-    
     let countOfGoods = goods.length;
    
       for (let i = 0; i < countOfGoods; i+=1) {
@@ -26,14 +25,13 @@ function sendRequest(name, phone, address, goods, sum) {
     }
 
     data.order.address =`ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, 
-                         ${address.floor} этаж, ${address.flat} квартира`;
+                         ${address.floor} этаж, кв ${address.flat}`;
         
     data.order.sum = sum; 
 
     data.client = `'${name} ${phone}'`;
 
-    let jsonData = JSON.stringify({data});
-
+    let jsonData = JSON.stringify({data: data});
     return jsonData;
 }
 
@@ -66,18 +64,19 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
    
       for (let i = 0; i < countOfGoods; i++) {
-        data.goods.push(goods[i].title, goods[i].count);
+        data.goods.push(goods[i].title);
+        data.goods.push(goods[i].count);
         
     }
 
     data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, 
-                         ${address.floor} этаж, ${address.flat} квартира`;
+                         ${address.floor} этаж, кв ${address.flat}`;
         
     data.order.sum = sum; 
 
     data.client = `'${name} ${phone}'`;
 
-    let jsonData = JSON.stringify({data});
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 };
