ee5b0735d11e68dce9e0f1705b53d8fb3d3509b9
Daniel <reconyk@gmail.com>
2022-05-24 14:51:45 +0300

    20 added

--- task_final/solution.js
+++ task_final/solution.js
@@ -20,7 +20,8 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
    
       for (let i = 0; i < countOfGoods; i+=1) {
-        data.goods.push(goods[i].title, goods[i].count);
+        data.goods.push(goods[i].title);
+        data.goods.push(goods[i].count);
 
     }
 
